aff41dbf5366cf329fecc02a5f6d190157134bce
KAIQUE ONENCIO DE SOUZA <0201392321040>
2025-09-29 11:34:29 -0300

    Aula de Delegate

---
+++ semestre_3/aula_06/Delegate/02_Delegate_Evento/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine("Iniciando o Pedido");
+Pedido pedido = new Pedido();
+
+// Registra os assinantes - relaciona as classes como se fossem 'triggers'
+pedido.OnPedidoPendente += EnviarEmail.EmailPendente; // -= caso não necessite de algum
+pedido.OnPedidoPendente += EnviarSMS.SMSPendente;
+
+pedido.OnCriarPedido += EnviarEmail.Email; 
+pedido.OnCriarPedido += EnviarSMS.SMS;
+
+// Criar o pedido - ao ser criado, realiza os 'triggers'
+pedido.PedidoPendente();
+pedido.CriarPedido();
+
+Console.ReadKey();
+
+public delegate void PedidoEvent();
+class Pedido
+{
+    public event PedidoEvent? OnCriarPedido;
+    public event PedidoEvent? OnPedidoPendente;
+    public void CriarPedido() 
+    {
+        if (OnCriarPedido != null && OnPedidoPendente != null)
+        {
+            OnCriarPedido();
+
+            Console.WriteLine("Pedido sendo criado...");
+            Console.WriteLine("Finalizando o pedido");
+        }  
+    }
+
+    public void PedidoPendente() 
+    {
+        if (OnPedidoPendente != null)
+        {
+            OnPedidoPendente();
+        }
+    }
+}
+
+class EnviarEmail
+{
+    public static void Email()
+    {
+        Console.WriteLine("E-mail confirmado!");
+    }
+
+    public static void EmailPendente()
+    {
+        Console.WriteLine("E-mail enviado! Por favor, confirme.");
+    }
+}
+
+class EnviarSMS
+{
+    public static void SMS()
+    {
+        Console.WriteLine("SMS confirmado!");
+    }
+
+    public static void SMSPendente()
+    {
+        Console.WriteLine("SMS enviado! Por favor, confirme");
+    }
+}
---
+++ semestre_3/aula_06/Delegate/Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿// Forma 1
+MeuDelegate del1 = new MeuDelegate(MeuMetodo);
+
+// Forma 2
+MeuDelegate del2 = MeuMetodo;
+
+// Forma 3
+MeuDelegate del3 = (msg) => { return msg; };
+
+del1("Bom dia");
+del2("Boa tarde");
+del3("Boa noite");
+
+// Assinatura: mesmo retorno e tipo de parâmetro
+static string MeuMetodo(string mensagem)
+{
+    return mensagem;
+}
+
+Console.ReadKey();
+
+// Sem o MAIN, o Delegate deve ficar aqui embaixo
+public delegate string MeuDelegate(string msg);
