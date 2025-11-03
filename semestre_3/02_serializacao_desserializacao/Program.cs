65b82d091e725151e531dce9b1f84643af995f65
KAIQUE ONENCIO DE SOUZA <0201392321040>
2025-11-03 09:26:39 -0300

    Serialização e Desserialização

---
+++ semestre_3/02_serializacao_desserializacao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+var pessoa = new Pessoa { Nome = "Maria", Idade = 21 };
+
+// Serialização
+string json = System.Text.Json.JsonSerializer.Serialize(pessoa);
+Console.WriteLine(json);
+
+// Desserialização - a barra inversa é sempre usada antes de um caractere especial
+var jsonString = "{\"Nome\":\"João\",\"Idade\":25}";
+Pessoa PessoaDesserializada = System.Text.Json.JsonSerializer.Deserialize<Pessoa>(jsonString);
+Console.WriteLine(PessoaDesserializada.Nome);
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public Pessoa() { }
+
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
---
+++ semestre_3/aula_09/Program.cs
@@ -0,0 +1,19 @@
+﻿// Definindo o caminho
+var caminho = @"C:\Users\0201392321040\Desktop\caminho\arquivo1.txt";
+
+// Se o arquivo existir, permite a criação. Evita repetições.
+if (!File.Exists(caminho))
+{
+    // Preenche o arquivo com o que for dado, nesse caso: 'Autor Desconhecido'
+    File.WriteAllText(caminho, "Autor Desconhecido");
+}
+
+// Lê todo o conteúdo do arquivo que for passado para ele
+string conteudo = (File.ReadAllText(caminho));
+
+// Adiciona um novo texto no arquivo
+var novoTexto = "\r\nQuem canta seus males espanta" + Environment.NewLine +
+"Água mole em pedra dura tanto bate até que fura\r\nCasa de ferreiro espeto é de pau";
+File.AppendAllText(caminho, novoTexto);
+
+Console.WriteLine(conteudo);
